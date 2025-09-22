645124e0a96237067d09464a5b787747b59d21c6
wrpierre11 <wrpierre.bim.arqinge@gmail.com>
2025-09-22 15:09:26 -0500

    Barra de progreso

---
+++ static/script.js
@@ -0,0 +1,7 @@
+document.getElementById('pdf-form').addEventListener('submit', function() {
+    document.getElementById('pdf-loading-message').style.display = 'block';
+});
+
+document.getElementById('merge-form').addEventListener('submit', function() {
+    document.getElementById('merge-loading-message').style.display = 'block';
+});
