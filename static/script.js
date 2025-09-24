226393f971b979c591b1ba3c76b0033136f044be
wrpierre11 <wrpierre.bim.arqinge@gmail.com>
2025-09-24 14:37:23 -0500

    Eliminado archivo script.js de la carpeta static

--- static/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('pdf-form').addEventListener('submit', function() {
-    document.getElementById('pdf-loading-message').style.display = 'block';
-});
-
-document.getElementById('merge-form').addEventListener('submit', function() {
-    document.getElementById('merge-loading-message').style.display = 'block';
-});
